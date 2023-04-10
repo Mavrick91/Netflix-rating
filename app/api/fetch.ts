8b0caf2658a8921b5fb9ff35e1a98535ca4f65cf
Shady <clownclowed@gmail.com>
2023-04-10 19:17:41 +0800

    Throw error if `process.env.RAPID_API_KEY` and `process.env.RAPID_API_HOST` are not set

--- app/api/fetch.ts
+++ app/api/fetch.ts
@@ -1,10 +1,17 @@
 const customFetch = (endpoint: string) => {
+  if (!process.env.RAPID_API_KEY || !process.env.RAPID_API_HOST) {
+    throw new Error(
+        "RAPID_API_KEY and RAPID_API_HOST environment variables are required"
+    );
+  }
+
+  const headers = new Headers();
+  headers.append("X-RapidAPI-Key", process.env.RAPID_API_KEY);
+  headers.append("X-RapidAPI-Host", process.env.RAPID_API_HOST);
+
   return fetch(`https://streaming-availability.p.rapidapi.com/v2${endpoint}`, {
     method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "49f9a941f4mshfa3fccf89e77ff1p1b4c23jsn5ca7f27682af",
-      "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-    },
+    headers,
   });
 };
 
