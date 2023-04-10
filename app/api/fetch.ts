a400ce8ad538b2532545339479aa94473520c5e2
Shady <clownclowed@gmail.com>
2023-04-10 21:36:12 +0800

    Stop using .env for fetch

--- app/api/fetch.ts
+++ app/api/fetch.ts
@@ -1,13 +1,10 @@
 const customFetch = (endpoint: string) => {
-  if (!process.env.RAPID_API_KEY || !process.env.RAPID_API_HOST) {
-    throw new Error(
-        "RAPID_API_KEY and RAPID_API_HOST environment variables are required"
-    );
-  }
-
   const headers = new Headers();
-  headers.append("X-RapidAPI-Key", process.env.RAPID_API_KEY);
-  headers.append("X-RapidAPI-Host", process.env.RAPID_API_HOST);
+  headers.append(
+    "X-RapidAPI-Key",
+    "49f9a941f4mshfa3fccf89e77ff1p1b4c23jsn5ca7f27682af"
+  );
+  headers.append("X-RapidAPI-Host", "streaming-availability.p.rapidapi.com");
 
   return fetch(`https://streaming-availability.p.rapidapi.com/v2${endpoint}`, {
     method: "GET",
